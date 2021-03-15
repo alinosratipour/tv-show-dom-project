40f0fb7f5d7ea849b963b117bed11c3058e161cf
alinosratipour <alinosratipour@gmail.com>
2021-03-15 01:35:12 +0000

    finished level 100
    I still working on it to improve it

--- script.js
+++ script.js
@@ -6,7 +6,36 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s) test`;
+  const grid = document.createElement("div");
+
+  grid.className = "box";
+
+  episodeList.map((item) => {
+    // create element
+    const card = document.createElement("div");
+    const h3 = document.createElement("h3");
+    const img = document.createElement("img");
+    const movieInfo = document.createElement("p");
+
+    //Append child elements to their parents
+    rootElem.appendChild(grid);
+    grid.appendChild(card);
+    card.appendChild(h3);
+    card.appendChild(img);
+    card.appendChild(movieInfo);
+
+    //add class to elements
+    card.className = "movieCard";
+    h3.className = "movieTitle";
+    movieInfo.className = "summery";
+    img.className = "img";
+
+    // display data on the page
+    h3.innerHTML = `  ${item.name} - S0${item.season}E0${item.number} `;
+    img.src = `${item.image.medium}`;
+    movieInfo.innerHTML = `${item.summary}`;
+    return item;
+  });
 }
 
 window.onload = setup;
