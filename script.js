07798fa60cae6d65462b859f2f32d95741533516
alinosratipour <alinosratipour@gmail.com>
2021-03-17 21:34:35 +0000

    finished up to 200 level
    I still need to add minor work to level 200 like how many record was shown after serach.

--- script.js
+++ script.js
@@ -4,91 +4,40 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
-const filteredMovies = getAllEpisodes();
-const rootElem = document.getElementById("root");
-const header = document.createElement("div");
-let searchBar = document.createElement("input");
-const grid = document.createElement("div");
-grid.className = "box";
-header.appendChild(searchBar);
-rootElem.appendChild(header);
-
-header.className = "header";
-searchBar.className = "searchBar";
-
-
-
-// searchBar.addEventListener("keyup", (e) => {
-//   const result = e.target.value;
-//   const filtered = filteredMovies.filter((movie) => {
-//   return movie.name.includes(result) || movie.summary.includes(result);
-   
-//   });
-//   console.log(filtered);
-
-//   return filtered;
-// });
-
-
-
-// function ali(){
-//   const res = searchBar.value;
- 
-//  const output = filteredMovies.filter((movie) => {
-//    return movie.name.includes(res) || movie.summary.includes(res);
-//  });
- 
- 
-
- 
-//}
-
-
-
-
-
-
-//searchBar.addEventListener('keyup', ali);
-
 function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+  let html = "";
 
-if(!searchBar.value){
   episodeList.map((item) => {
-    // create element
-    const card = document.createElement("div");
-    const h3 = document.createElement("h3");
-    const img = document.createElement("img");
-    const movieInfo = document.createElement("p");
-
-    //Append child elements to their parents
-    rootElem.appendChild(grid);
-    grid.appendChild(card);
-    card.appendChild(h3);
-    card.appendChild(img);
-    card.appendChild(movieInfo);
-
-    //add class to elements
-    card.className = "movieCard";
-    h3.className = "movieTitle";
-    movieInfo.className = "summery";
-    img.className = "img";
-
-    // display data on the page
-    h3.innerHTML = `  ${item.name} - S0${item.season}E0${item.number} `;
-    img.src = `${item.image.medium}`;
-    movieInfo.innerHTML = `${item.summary}`;
-
-    //return item;
+    html += ` 
+          <div class="movieCard">
+              <h3 class="movieTitle">${item.name} - S0${item.season}E0${item.number}</h3>
+              <img class="img" src= ${item.image.medium} />
+              <div class="summary">${item.summary}</div>
+          </div>
+    `;
   });
+
+  rootElem.innerHTML = html;
 }
 
+const searchBar = document.querySelector(".searchBar");
+const header = document.querySelector(".header");
+const allEpisodes = getAllEpisodes();
 
 
-}
-     //return showAllMovies;
-   
-  
+  searchBar.addEventListener("keyup", (e) => {
+    let result = e.target.value.toLowerCase();
 
+    const filtered = allEpisodes.filter((movie) => {
+      return movie.name.toLowerCase().includes(result) || movie.summary.toLowerCase().includes(result);
  
+    });
+   
+    makePageForEpisodes(filtered)
+  
+  });
+
+
 
 window.onload = setup;
